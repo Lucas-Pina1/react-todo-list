75e7540f1b9bd16277c513714a33eefa0e865714
Lucas <lucas.pina@prodepa.pa.gov.br>
2023-01-25 11:59:31 -0300

    avançando

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import * as C from "./App.styles";
 import { Item } from "./@types/Item";
 
 import { ListItem } from "./components/ListItem";
+import { AddArea } from "./components/AddArea";
 
 export const App = () => {
   const [list, setList] = useState<Item[]>([
@@ -16,7 +17,7 @@ export const App = () => {
       <C.Area>
         <C.Header>Lista de tarefas</C.Header>
 
-        {/* Área de adicionar nova tarefa*/}
+        <AddArea />
 
         {list.map((item, index) => (
           <ListItem key={index} item={item} />
--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,5 +1,28 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  
+type ContainerProps = {
+  done: boolean;
+};
+
+export const Container = styled.div(
+  ({ done }: ContainerProps) =>
+    `
+  display: flex;
+  align-items: center;
+  background-color: #20212C;
+  padding: 10px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+
+  input {
+    width: 20px;
+    height: 20px;
+    margin-right: 50px;
+  }
+
+  label {
+    color: #ccc;
+    text-decoration: ${done ? "line-through" : "initial"}
+  }
 `
+);
